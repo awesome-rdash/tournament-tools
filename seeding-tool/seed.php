b4deaa71e3916059fdaec0c03d228c293201ca6c
Justin Rouzier <jrouzier@student.42.fr>
2017-01-15 13:44:08 +0100

    Adding caching system
    To avoid calling riot API each time

--- seeding-tool/seed.php
+++ seeding-tool/seed.php
@@ -29,7 +29,7 @@ function encode_name($name) {
 	$db_infos = parse_ini_file("cfg.ini");
 	$api_key = $db_infos['api_key'];
 
-	echo "<strong>Clé API utilisée : $api_key </strong>";
+	echo "<strong>Utilisez l'option refetch_players pour actualiser les rangs + joueurs inscrits </strong>";
 	echo "<br />";
 	echo "<br />";
 
@@ -74,105 +74,111 @@ function encode_name($name) {
 	print_r($playersName);
 	echo "<pre />";*/
 
-	//On va recuperer les IDs de tous les joueurs. 
-	//Limite API : 40 joueurs par call. - 8 calls necessaires pour 320 joueurs
+	//On va recuperer les IDs de tous les joueurs UNIQUEMENT si demande
+	if (isset($_GET['refetch_players']) && $_GET['refetch_players'] == true) {
 
-	function getIdFromNames($playersName, $playersToCheck) {
-		$playersToCheck = rtrim($playersToCheck, ',');
-		$api_call_path = "v1.4/summoner/by-name/" . $playersToCheck;
-		$result = api_call($api_call_path);
-		echo "Variable playerstocheck = $playersToCheck <br />";
-		//echo "Resultat du call API ID from name";
-		//print_r($result);
+		function getIdFromNames($playersName, $playersToCheck) {
+			$playersToCheck = rtrim($playersToCheck, ',');
+			$api_call_path = "v1.4/summoner/by-name/" . $playersToCheck;
+			$result = api_call($api_call_path);
+			echo "Variable playerstocheck = $playersToCheck <br />";
+			//echo "Resultat du call API ID from name";
+			//print_r($result);
 
-		$playersToCheck_array = explode(',', $playersToCheck);
+			$playersToCheck_array = explode(',', $playersToCheck);
 
-		$playersID = [];
-		foreach($playersToCheck_array as $player) {
-			if (array_key_exists($player, $result)) {
-				$id = $result[$player]['id'];
-				$playersID[$id] = array (
-					"name" => $playersName[$player]['name'],
-					"encoded_name" => $playersName[$player]['encoded_name'],
-					"team" => $playersName[$player]['team'],
-					"id" => $result[$player]['id'],
-					"summonerLevel" =>$playersName[$player]['summonerLevel'],
-					"ranked_solo" => "",
-					"note" => "");
-			} else {
-				echo "Le joueur <strong>$player</strong> est introuvable dans la BDD Riot<br />";
+			$playersID = [];
+			foreach($playersToCheck_array as $player) {
+				if (array_key_exists($player, $result)) {
+					$id = $result[$player]['id'];
+					$playersID[$id] = array (
+						"name" => $playersName[$player]['name'],
+						"encoded_name" => $playersName[$player]['encoded_name'],
+						"team" => $playersName[$player]['team'],
+						"id" => $result[$player]['id'],
+						"summonerLevel" =>$result[$player]['summonerLevel'],
+						"ranked_solo" => "",
+						"note" => "");
+				} else {
+					echo "Le joueur <strong>" . $playersName[$player]['name'] . "</strong> est introuvable dans la BDD Riot<br />";
+				}
 			}
+
+			return $playersID;
 		}
 
-		return $playersID;
-	}
+		$count = 0;
+		$playersToCheck = "";
+		$players = [];
+		foreach ($playersName as $player) {
+			if ($count === 39) {
+				$return_array = getIdFromNames($playersName, $playersToCheck);
+				//print_r($return_array);
 
-	$count = 0;
-	$playersToCheck = "";
-	$players = [];
-	foreach ($playersName as $player) {
-		if ($count === 39) {
+				$players = $players + $return_array;
+
+				$playersToCheck = "";
+				$count = 0;
+			} else {
+				$playersToCheck .= $player['encoded_name'] . ",";
+				$count++;
+			}
+		}
+		if ($count != 0) {
 			$return_array = getIdFromNames($playersName, $playersToCheck);
-			//print_r($return_array);
-
 			$players = $players + $return_array;
-
-			$playersToCheck = "";
-			$count = 0;
-		} else {
-			$playersToCheck .= $player['encoded_name'] . ",";
-			$count++;
 		}
-	}
-	if ($count != 0) {
-		$return_array = getIdFromNames($playersName, $playersToCheck);
-		$players = $players + $return_array;
-	}
-/*
-	echo "\$players apres hydratation de l'ID <pre>";
-	print_r($players);
-	echo "<pre />";
-*/
-	sleep(1); //reset de la limite de l'API Riot pour les cles de dev
+	/*
+		echo "\$players apres hydratation de l'ID <pre>";
+		print_r($players);
+		echo "<pre />";
+	*/
+		sleep(1); //reset de la limite de l'API Riot pour les cles de dev
 
-	//On a maintenant tous les IDs Riot des joueurs du tournoi, yay !
-	//On va recuperer les informations de leur niveau classe en solo
-	//Limite API : 10 joueurs par call. - 32 calls necessaires pour 320 joueurs
+		//On a maintenant tous les IDs Riot des joueurs du tournoi, yay !
+		//On va recuperer les informations de leur niveau classe en solo
+		//Limite API : 10 joueurs par call. - 32 calls necessaires pour 320 joueurs
 
-	function getLevelFromID(&$players, $playersToCheck) {
+		function getLevelFromID(&$players, $playersToCheck) {
 
-		$playersToCheck = rtrim($playersToCheck, ',');
-		$api_call_path = "v2.5/league/by-summoner/" . $playersToCheck . "/entry";
-		echo "APICALL : $api_call_path <br />";
-		$result = api_call($api_call_path);
-		//print_r($result);
+			$playersToCheck = rtrim($playersToCheck, ',');
+			$api_call_path = "v2.5/league/by-summoner/" . $playersToCheck . "/entry";
+			echo "APICALL : $api_call_path <br />";
+			$result = api_call($api_call_path);
+			//print_r($result);
 
-		$playersToCheck_array = explode(',', $playersToCheck);
-		foreach($playersToCheck_array as $player) {
-			if (array_key_exists($player, $result)) {
-				$players[$player]['ranked_solo'] = 
-				$result[strval($player)];
-			} else {
-				echo "Le joueur <strong>" . $players[$player]['name'] . "</strong> n'est pas classe en SOLO RANKED<br />";
+			$playersToCheck_array = explode(',', $playersToCheck);
+			foreach($playersToCheck_array as $player) {
+				if (array_key_exists($player, $result)) {
+					$players[$player]['ranked_solo'] = 
+					$result[strval($player)];
+				} else {
+					echo "Le joueur <strong>" . $players[$player]['name'] . "</strong> n'est pas classe en SOLO RANKED<br />";
+				}
 			}
 		}
+
+		$count = 0;
+		$playersToCheck = "";
+		foreach ($players as $player) {
+			if ($count === 9) {
+				getLevelFromID($players, $playersToCheck);
+				$playersToCheck = "";
+				$count = 0;
+			} else {
+				$playersToCheck .= $player['id'] . ",";
+				$count++;
+			}
+		}
+		if ($count != 0) {
+			getLevelFromID($players, $playersToCheck);
+		}
+
+		file_put_contents('data.bin', serialize($players));
+		unset($players);
 	}
 
-	$count = 0;
-	$playersToCheck = "";
-	foreach ($players as $player) {
-		if ($count === 9) {
-			getLevelFromID($players, $playersToCheck);
-			$playersToCheck = "";
-			$count = 0;
-		} else {
-			$playersToCheck .= $player['id'] . ",";
-			$count++;
-		}
-	}
-	if ($count != 0) {
-		getLevelFromID($players, $playersToCheck);
-	}
+	$players = unserialize(file_get_contents('data.bin'));
 
 	//On Calcule maintenant la note de chaque joueur
 	foreach($players as &$player) {
@@ -208,7 +214,7 @@ function encode_name($name) {
 				$player['note'] = 1;
 			}
 		} else {
-			$player['note'] = "Non note";
+			$player['note'] = "0";
 		}
 	}
 
@@ -242,29 +248,49 @@ function encode_name($name) {
 				return "green";
 			case 13:
 				return "green";
+			return "white";
 		}
+	}
+
+	//On va creer un tableau teams avec les informations des joueurs
+
+	function getTeamNote($players, $team) {
+		$playersNote = [];
+
+		foreach($players as $player) {
+			if ($player['note'] != 0 && $player['team'] == $team) {
+				$playersNote[] = $player['note'];
+			}
+		}
+		return (int)(ceil(array_sum($playersNote) / count($playersNote)));
 	}
 
 	// On affiche un super tableau maintenant
 
 ?>
 
-<table width="600px">
+<table width="1200px">
 	<tr>
 		<th>Pseudo</th>
 		<th>Equipe</th>
 		<th>Rang Solo</th>
 		<th>Note</th>
+		<th>Moyenne d'equipe</th>
 	</tr>
 <?php
-foreach ($players as $player) {
+$lastTeam = "";
+foreach ($players as $player) {	
+	$teamNote = getTeamNote($players, $player['team']);
+
 	echo "<tr>";
 	echo "<td>" . $player['name'] . "</td>";
 	echo "<td>" . $player['team'] . "</td>";
 	if (!empty($player['ranked_solo'][0]['tier'])) {
 		echo "<td>" . $player['ranked_solo'][0]['tier'] . " - Division " . $player['ranked_solo'][0]['entries'][0]['division'] . "</td>";
 	} else { echo "<td>Non classe</td>"; }
-	echo "<td style=\"text-decoration:underline; background-color: ". get_background_color($player['note']) .";\" ><mark>" . $player['note'] . "</mark></td>";
+	echo "<td style=\"text-decoration:underline; text-align:center; background-color: ". get_background_color($player['note']) .";\" ><mark>" . $player['note'] . "</mark></td>";
+	echo "<td style=\"text-decoration:underline; text-align:center; background-color: ". get_background_color($teamNote) .";\" ><mark>" . $teamNote . "</mark></td>";
+	echo "";
 
 	echo "<tr />";
 }
